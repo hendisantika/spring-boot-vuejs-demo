9cbb24987942e3f3865a997d0ab5a890dde2557c
Hendi Santika <hendisantika@gmail.com>
2021-07-04 10:17:07 +0700

    #7 - Add Lend Entity

---
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/entity/Lend.java
@@ -0,0 +1,45 @@
+package com.hendisantika.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.print.Book;
+import java.time.Instant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/07/21
+ * Time: 10.16
+ */
+@Data
+@Entity
+@Table(name = "lend")
+@NoArgsConstructor
+public class Lend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.ORDINAL)
+    private LendStatus status;
+
+    private Instant startOn;
+    private Instant dueOn;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    @JsonManagedReference
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    @JsonManagedReference
+    private Member member;
+}
