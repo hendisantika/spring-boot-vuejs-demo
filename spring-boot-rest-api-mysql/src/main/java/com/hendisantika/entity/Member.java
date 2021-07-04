41547398d208c7f1e1908f2e4c724d2ea4b26566
Hendi Santika <hendisantika@gmail.com>
2021-07-04 10:14:34 +0700

    #5 - Add Member Entity

---
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/entity/Member.java
@@ -0,0 +1,37 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/07/21
+ * Time: 10.13
+ */
+@Data
+@Entity
+@Table(name = "member")
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+}
