83c9dd4e92398f86f01dc82d329d16d0fa440f72
Hendi Santika <hendisantika@gmail.com>
2021-07-04 10:15:32 +0700

    #6 - Add LendStatus Enum

---
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/entity/LendStatus.java
@@ -0,0 +1,14 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/07/21
+ * Time: 10.15
+ */
+public enum LendStatus {
+    AVAILABLE, BURROWED
+}
