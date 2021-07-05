6bca233bb03a86db023fbad3b6b9d461fe9dda38
Hendi Santika <hendisantika@gmail.com>
2021-07-05 07:15:27 +0700

    #31 - Add readBook by ISBN in LibraryController

--- spring-boot-rest-api-mysql/src/main/java/com/hendisantika/controller/LibraryController.java
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/controller/LibraryController.java
@@ -2,8 +2,11 @@ package com.hendisantika.controller;
 
 import com.hendisantika.service.LibraryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,5 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryController {
 
     private final LibraryService libraryService;
+
+    @GetMapping("/book")
+    public ResponseEntity readBooks(@RequestParam(required = false) String isbn) {
+        if (isbn == null) {
+            return ResponseEntity.ok(libraryService.readBooks());
+        }
+        return ResponseEntity.ok(libraryService.readBook(isbn));
+    }
 
 }
