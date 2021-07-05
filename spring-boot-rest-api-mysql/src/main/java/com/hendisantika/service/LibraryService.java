ac2023325a8297936949648c67d9cc1a118ddc54
Hendi Santika <hendisantika@gmail.com>
2021-07-05 07:08:47 +0700

    #21 - Add readBook By ISBN in LibraryService

--- spring-boot-rest-api-mysql/src/main/java/com/hendisantika/service/LibraryService.java
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/service/LibraryService.java
@@ -41,4 +41,12 @@ public class LibraryService {
     public List<Book> readBooks() {
         return bookRepository.findAll();
     }
+
+    public Book readBook(String isbn) {
+        Optional<Book> book = bookRepository.findByIsbn(isbn);
+        if (book.isPresent()) {
+            return book.get();
+        }
+        throw new EntityNotFoundException("Cant find any book under given ISBN");
+    }
 }
