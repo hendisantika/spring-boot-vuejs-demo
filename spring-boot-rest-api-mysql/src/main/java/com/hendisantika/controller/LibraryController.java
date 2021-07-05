d1aca68442a9d60d3683ac5828d776cc718de4eb
Hendi Santika <hendisantika@gmail.com>
2021-07-05 07:15:59 +0700

    #32 - Add readBook in LibraryController

--- spring-boot-rest-api-mysql/src/main/java/com/hendisantika/controller/LibraryController.java
+++ spring-boot-rest-api-mysql/src/main/java/com/hendisantika/controller/LibraryController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,11 @@ public class LibraryController {
             return ResponseEntity.ok(libraryService.readBooks());
         }
         return ResponseEntity.ok(libraryService.readBook(isbn));
+    }
+
+    @GetMapping("/book/{bookId}")
+    public ResponseEntity<Book> readBook(@PathVariable Long bookId) {
+        return ResponseEntity.ok(libraryService.readBook(bookId));
     }
 
 }
